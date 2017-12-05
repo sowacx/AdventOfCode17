27ac0ec4cf53e190283c3980f3a156b2ac3ad622
Michał Witczak <mich.witczak@gmail.com>
2017-12-05 23:35:44 +0100

    Day5

---
+++ AoC_17_RestOf/AoC_17_RestOf_Test/UnitTest51.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AoC_17_RestOf;
+
+namespace AoC_17_RestOf_Test
+{
+    [TestClass]
+    public class UnitTest51
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = @"0
+3
+0
+1
+-3";
+            int solution = PuzzleSolver.Solve51(input);
+            Assert.AreEqual(solution, 5);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string input = @"0
+0
+0
+0
+0";
+            int solution = PuzzleSolver.Solve51(input);
+            Assert.AreEqual(solution, 10);
+        }
+       
+    }
+}
